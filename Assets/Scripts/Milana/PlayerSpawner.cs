1a98432c2213741377e7c551dffd737342c187f5
mtrats <tratsevskam@ufl.edu>
2026-03-30 15:21:26 -0400

    Set up basic multiplayer
    Also added folders

---
+++ Assets/Scripts/Milana/PlayerSpawner.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
+{
+    public GameObject PlayerPrefab;
+
+    void IPlayerJoined.PlayerJoined(PlayerRef player)
+    {
+        if (player == Runner.LocalPlayer)
+        {
+            Runner.Spawn(PlayerPrefab, new Vector3(-17.29f, 8.6f, 10f), Quaternion.identity);
+        }
+    }
+}
